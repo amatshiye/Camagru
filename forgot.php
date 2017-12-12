b2bb7a098f2896160720be605e86e07b813e9ca5
amatshiye <amatshiy@student.42.fr>
2017-12-12 04:48:39 +0200

    user settings feature added

--- forgot.php
+++ forgot.php
@@ -44,6 +44,7 @@ else if (isset($_GET['code']) && $_GET['code'] == -1)
   <div class="topnav" id="myTopnav">
     <a href="index.php">Home</a>
     <a href="gallery.php">Gallery</a>
+    <a href="settings.php">Settings</a>
     <a href="index.php?user=log">Logout</a>
     <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
   </div>
